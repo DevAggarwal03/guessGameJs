fcd2f4978c43a7eb0e860fd20cc2171d10c064c8
Dev Aggarwal <devaggarwal1009@gmail.com>
2024-02-28 17:59:01 +0530

    commited

---
+++ sctipt.js
@@ -0,0 +1,44 @@
+let RNum = Math.floor(Math.random()*100) +1;
+const num = document.getElementById("num");
+let submit = document.getElementById("submit");
+let guesses = document.getElementById("guesses")
+let hint = document.getElementById("hint")
+let guess = 0;
+let GNum;
+let running = truee;
+
+// console.log(RNum);
+
+while(running){
+    submit.onclick = function(){
+        GNum = num.value;
+        GNum = Number(GNum);
+        if(isNaN(GNum)){
+            hint.textContent = `Enter a Valid Number`;
+            console.log(GNum)
+        }
+        else if(GNum > 100 || GNum <= 0){
+            hint.textContent = `Enter a Valid Number`;
+            console.log(GNum)
+        }
+        else{
+            if(GNum > RNum){
+                hint.textContent = `Go lower`;
+                guess += 1;
+                guesses.textContent = guess;
+                console.log(GNum)
+            }
+            else if(GNum < RNum){
+                hint.textContent = `Go higher`;
+                guess += 1;
+                guesses.textContent = guess;
+                console.log(GNum)
+            }
+            else{
+                hint.textContent = `Congratulations you compleated the game in ${guess} guesses`;
+                console.log(GNum)
+                running = false;
+            }
+        }
+    }
+}
